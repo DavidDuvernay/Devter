056e3742866f483c7637815934cdf5d966112575
David Duvernay <david.anderson0612@gmail.com>
2023-09-04 11:54:26 -0400

    By passing firebase admin method to return plain object

--- firebase/admin.js
+++ firebase/admin.js
@@ -1,17 +1,28 @@
-import { getApps, initializeApp } from "firebase/app";
-import firebaseConfig from "./firebaseConfig";
+// import { getApps, initializeApp } from "firebase/app";
+// import firebaseConfig from "./firebaseConfig";
 
-var admin = require("firebase-admin");
+// var admin = require("firebase-admin");
 
-try {
-  if ( !getApps().length ) initializeApp(firebaseConfig)
+// try {
+//   if ( !getApps().length ) initializeApp(firebaseConfig)
 
-  var serviceAccount = JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_SERVICE_ACCOUNT_KEY)
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: process.env.FIREBASE_DATABASE_URL
-  });
-}
-catch (e) {}
+//   var serviceAccount = JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_SERVICE_ACCOUNT_KEY)
+//   admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount),
+//     databaseURL: process.env.FIREBASE_DATABASE_URL
+//   });
+// }
+// catch (e) {}
 
-export const firestore = admin.firestore();
+// export const firestore = admin.firestore();
+export const firestore =  {
+    id: "7aJxkA2oftcWn9PoZNVl",
+    createdAt: 1692324897304,
+    likesCount: 0,
+    img: null,
+    avatar: 'https://avatars.githubusercontent.com/u/20390970?v=4',
+    userName: 'David Duvernay',
+    userId: 'fj3lb0X4OAducuKPAXiCd0I3IfD2',
+    content: 'did the databaseURL changed? ☁️💻',
+    sharedCount: 0
+  };
--- pages/status/[id].js
+++ pages/status/[id].js
@@ -1,48 +1,50 @@
-// import Devit from "@/componets/Devit";
-// import { firestore } from "@/firebase/admin";
-// import { useRouter } from "next/router";
+import Devit from "@/componets/Devit";
+import { firestore } from "@/firebase/admin";
+import { useRouter } from "next/router";
 
-// export async function getStaticPaths() {
-//   return {
-//     paths: [{ params: { id: "aq9gzq5yyrKiXxL92FeI" } }],
-//     fallback: true
-//   }
-// };
+export async function getStaticPaths() {
+  return {
+    paths: [{ params: { id: "aq9gzq5yyrKiXxL92FeI" } }],
+    fallback: true
+  }
+};
 
-// export async function getStaticProps(ctx) {
-//   const { params, res } = ctx;
-//   const { id } = params;
+export async function getStaticProps(ctx) {
+  const { params, res } = ctx;
+  const { id } = params;
 
-//   return await firestore
-//     .collection("devits")
-//     .doc(id)
-//     .get()
-//     .then((doc) => {
-//       const data = doc.data();
-//       const id = doc.id;
-//       const { createdAt } = data;
+  // return await firestore
+  //   .collection("devits")
+  //   .doc(id)
+  //   .get()
+  //   .then((doc) => {
+  //     const data = doc.data();
+  //     const id = doc.id;
+  //     const { createdAt } = data;
+  //     console.log(data)
+  //     const props = {
+  //       ...data,
+  //       id,
+  //       createdAt: +createdAt.toDate()
+  //     };
+  //     console.log(props.createdAt)
 
-//       const props = {
-//         ...data,
-//         id,
-//         createdAt: +createdAt.toDate()
-//       };
+  //     return { props }
+  //   })
+  //   .catch(() => {
+  //     return { props: {} }
+  //   })
+  return {props: firestore}
+};
 
-//       return { props }
-//     })
-//     .catch(() => {
-//       return { props: {} }
-//     })
-// };
+export default function DevitPage(props) {
+  const router = useRouter();
 
-// export default function DevitPage(props) {
-//   const router = useRouter();
+  if (router.isFallback) return <h1>Loading...</h1>
 
-//   if (router.isFallback) return <h1>Loading...</h1>
-
-//   return (
-//     <>
-//       <Devit {...props}></Devit>
-//     </>
-//   )
-// };
+  return (
+    <>
+      <Devit {...props}></Devit>
+    </>
+  )
+};
